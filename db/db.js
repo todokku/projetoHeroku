6fa13d08ba9613bb4b551ba4c7142409c7a0ebe7
Andrey Lobo <suporte01@socarrao.com.br>
2020-04-15 17:12:25 -0300

    ajuste senha pg

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ module.exports = function(){
     client = new Pool({
         host:'ec2-52-70-15-120.compute-1.amazonaws.com',
         user:'yksslihsytfwzt',
-        password:'5aa55d331bd064f6e865f6334135ace824bca1d04e8304bc6f6836b3d64a9ef1345',
+        password:'5aa55d331bd064f6e865f6334135ace824bca1d04e8304bc6f6836b3d64a9ef1',
         database:'dcnh2h8ub457o6'
     });
     return client.connect();
