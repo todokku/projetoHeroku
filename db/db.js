de2f19de6486b3a93f7cc036e105d698641eb54a
Andrey Lobo <suporte01@socarrao.com.br>
2020-04-15 17:10:21 -0300

    teste com pool

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 // db.js
-const {Client} = require('pg');
+const { Pool, Client } = require('pg');
 
 module.exports = function(){
-    client = new Client({
+    client = new Pool({
         host:'ec2-52-70-15-120.compute-1.amazonaws.com',
         user:'yksslihsytfwzt',
         password:'5aa55d331bd064f6e865f6334135ace824bca1d04e8304bc6f6836b3d64a9ef1345',
