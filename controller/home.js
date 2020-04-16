ab7ce00d3fe7c0b1f0d2185e02dfdc1a6bf0742c
Andrey Lobo <suporte01@socarrao.com.br>
2020-04-16 10:51:24 -0300

    teste producao

--- controller/home.js
+++ controller/home.js
@@ -2,7 +2,7 @@ const { check, validationResult } = require('express-validator');
 module.exports = function(app){
       var result ={
             index: function(req,res){
-                  var connection =  app.db.db.on();
+                  var connection =  app.db.db;
                   var origem = req.path.replace( '/', '');;
                   var home = new app.model.Noticias;
                   console.log(connection);
